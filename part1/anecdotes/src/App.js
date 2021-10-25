124abe2a6159070ac5db76dedf3e2bc3ef3f8d8d
vjerkko <vjerkko@gmail.com>
2021-10-25 19:02:34 +0300

    {Exercise 1.14 finished}

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -6,6 +6,9 @@ const Button = ({handleClick, text}) => (
   </button> 
 )
 
+//Returns max value from votes array
+const findMax = votes => votes.indexOf(Math.max(...votes))
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -20,24 +23,29 @@ const App = () => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(new Uint8Array(7))
 
-  const handleSelect = () => setSelected(generateRnd())
-  const handleUpdate = () => { 
+  const handleSelect = () => setSelected(generateRnd()) //Generates random number 0-6
+  const handleUpdate = () => { //Updates array containing votes
     const newVotes = [...votes]
     newVotes[selected] += 1
     setVotes(newVotes)
+    
   }
-  
+  console.log(votes)
+  let max = findMax(votes)
+  console.log("Max: ", max)
   const generateRnd = () => Math.floor(Math.random() * 7)
-  
-  console.log("Selected anecdote: ", selected)
-  console.log("votes ",  votes)
     
   return (
     <div>
+      <h1>Anecdote of the Day</h1>
       {anecdotes[selected]}<br></br>
       <Button handleClick={handleUpdate} text="vote" />
       <Button handleClick={handleSelect} text="Next anectdote"/>
-      
+      <div>
+      <h1>Anecdote With Most Votes</h1>
+      {anecdotes[max]}<br></br>
+      has {votes[max]} votes
+      </div>
     </div>
   )
 }
