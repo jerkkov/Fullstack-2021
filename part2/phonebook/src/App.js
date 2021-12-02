b274fa5b04f13c510b0c2ad6bc3327bb71d50428
vjerkko <vjerkko@gmail.com>
2021-12-02 15:38:26 +0200

    {Exercise 2.11 finished}

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Addperson from './components/Addperson'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    {id: 0, name: 'Arto Hellas', number: '040-123456' },
-    {id: 1, name: 'Ada Lovelace', number: '39-44-5323523' },
-    {id: 2, name: 'Dan Abramov', number: '12-43-234345' },
-    {id: 3, name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+const App = () => { 
   const [showAll, setShowAll] = useState('')
-  const [newName, setNewName] = useState(
-    ''
-    )
-  const [newNumber, setNewNumber] = useState(
-    ''
-  )
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+    console.log('promise fulfilled')
+    setPersons(response.data)
+    })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
     //Inputfield management for name
   const handleNameChange = (event) => {
