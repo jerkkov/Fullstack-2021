2596b39e892cb0166e328f7afdd60f0132b30926
vjerkko <vjerkko@gmail.com>
2021-12-10 00:47:33 +0200

    {Part 3.2 Finished}

--- personsbackend/index.js
+++ personsbackend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
       name: "Arto Hellas",
@@ -11,11 +13,75 @@ let persons = [
       name: "Ada Lovelace",
       number: "39-44-5323523",
       id: 2
+    },
+
+    {
+      name: "Paska PErse",
+      number: "39-44-5323523",
+      id: 3
     }
   ]
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  app.get('/api/persons/info', (request, response) => {
+      const personsCount = persons.length
+      date = new Date()
+      response.send(
+        '<p>Phonebook has info for ' + personsCount + ' people </p> <p>' + 
+        date +'</p>')
+  })
+
+  app.get('/api/persons', (request, response) => {
+    if(persons) {
+      response.json(persons)
+    } else {
+      response.status(404).end()
+    }
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if(person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
+
+const generateId = () => {
+  const maxId = persons.length > 0
+  ? Math.max(...persons.map(n => n.id))
+  : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if(!body.name) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  
+  response.status(204).end()
+})
+
+
 
 const PORT = 3001
 app.listen(PORT)
