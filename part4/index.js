ab0b7078c01a3368011b80a5820a8ebcf088f16e
jerkko.viisteensaari <jerkko.viisteensaari@insta.fi>
2022-06-17 15:44:04 +0300

    Add blog model, connecting uses .env file

--- part4/index.js
+++ part4/index.js
@@ -1,20 +1,11 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
+require('dotenv').config()
+const Blog = require ('./models/blog')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
@@ -28,7 +19,12 @@ app.get('/api/blogs', (request, response) => {
 })
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
 
   blog
     .save()
@@ -36,8 +32,7 @@ app.post('/api/blogs', (request, response) => {
       response.status(201).json(result)
     })
 })
-
-const PORT = 3003
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
