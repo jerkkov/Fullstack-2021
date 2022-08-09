1b4b748381668d3a056fc0109f44d68a22c99949
jerkko.viisteensaari <jerkko.viisteensaari@insta.fi>
2022-08-09 10:45:42 +0300

    4.2 finished

--- part4/index.js
+++ part4/index.js
@@ -1,51 +1,10 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const Blog = require('./models/blog')
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-  }
+const server = http.createServer(app)
 
-app.use(express.json())
-
-app.use(requestLogger)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      if (blogs) {
-        response.json(blogs)
-      }
-      else {
-        response.status(404).end()
-      }
-      
-    }).catch(error => console.log(error))
-})
-
-app.post('/api/blogs', (request, response) => {
-  const body = request.body
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  })
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
