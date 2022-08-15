cb66d9aeca5c07d66092ba59c91c99d033fbdb4e
jerkko.viisteensaari <jerkko.viisteensaari@insta.fi>
2022-08-15 09:38:09 +0300

    4.5 finished

--- part4/utils/blog.test.js
+++ part4/utils/blog.test.js
@@ -1,4 +1,5 @@
 const listHelper = require('../utils/list_helper')
+const sample = require('./sampleBlogs')
 
 test('dummy returns one', () => {
   const blogs = []
@@ -24,3 +25,19 @@ describe('total likes', () => {
       expect(result).toBe(5)
     })
   })
+
+  describe('favoriteBlog', () => {
+    test('When list has multiple blogs, return one which has most likes', () => {
+      const result = listHelper.favoriteBlog(sample.blogs)
+      expect(result).toEqual( 
+      {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+      })
+    })
+  })
+
