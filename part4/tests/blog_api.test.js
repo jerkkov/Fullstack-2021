1ed5c98ef6aede2611fdd775845e43251d3c0f6d
jerkko.viisteensaari <jerkko.viisteensaari@insta.fi>
2022-08-16 10:33:45 +0300

    4.9 finished

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -12,13 +12,22 @@ beforeEach(async () => {
     await Blog.insertMany(initialBlogs)
 })
 
-    test('blogs are returned as json and expected amount is returned', async () => {
-        const response = await api.get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-        expect(response.body).toHaveLength(initialBlogs.length)
-        
-    })
+test('blogs are returned as json and expected amount is returned', async () => {
+    const response = await api.get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+    expect(response.body).toHaveLength(initialBlogs.length)
+    
+})
+
+test('id field is defined', async () => {
+    const response = await api.get('/api/blogs')
+    for (let i = 0; i < response.length; i++) {    
+    expect(blog._id[i]).toBeDefined()
+    }
+    
+    
+})
 
 afterAll(() => {
     mongoose.connection.close()
