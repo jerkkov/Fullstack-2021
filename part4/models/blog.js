bfa65e3ae4b0e3d9c34e845f789539ecae848dc0
vjerkko <vjerkko@gmail.com>
2022-12-31 15:09:30 +0200

    4.10 Finished

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-    _id: String,
+    // _id: String,
     title: String,
     author: String,
     url: String,
-    likes: Number
+    likes: Number,
+  })
+  
+  blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
   })
 
   module.exports = mongoose.model('Blog', blogSchema)
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,54 +1,54 @@
 const mongoose = require ('mongoose')
 const supertest = require ('supertest')
+const helper = require('../utils/sampleBlogs')
 const app = require('../app')
-
 const api = supertest(app)
+
 const Blog = require('../models/blog')
 
-const initialBlogs = require('../utils/sampleBlogs').blogs
+describe('when there is initially some blogs saved', () => {
+    beforeEach(async () => {
+        await Blog.deleteMany({})
+        await Blog.insertMany(helper.blogs)
+    }, 100000)
 
-beforeEach(async () => {
-    await Blog.deleteMany({})
-    await Blog.insertMany(initialBlogs)
+    test('blogs are returned as json and expected amount is returned', async () => {
+        const response = await api.get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+        expect(response.body).toHaveLength(helper.blogs.length)
+    })
+
+    test('id field is defined', async () => {
+        const response = await api.get('/api/blogs')
+        for (let i = 0; i < response.length; i++) {    
+        expect(response._id[i]).toBeDefined()
+        }
+    })
+
+    test('a valid blog can be added', async () => {
+        const newBlog = {
+            id: "5a422b891b54a676234d34ds",
+            title: "Second class tests",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html",
+            likes: 10,
+        }
+
+        await api 
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+        const response = await api.get('/api/blogs')
+
+        const contents = response.body.map(r => r.title)
+
+        expect(response.body).toHaveLength(helper.blogs.length + 1)
+        expect(contents).toContain('Second class tests')
+    })
 })
-
-test('blogs are returned as json and expected amount is returned', async () => {
-    const response = await api.get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-    expect(response.body).toHaveLength(initialBlogs.length)
-})
-
-test('id field is defined', async () => {
-    const response = await api.get('/api/blogs')
-    for (let i = 0; i < response.length; i++) {    
-    expect(response._id[i]).toBeDefined()
-    }
-})
-
-test('a valid blog can be added', async () => {
-    const newBlog = {
-        _id: "5a422b891b54a676234d34ds",
-        title: "Second class tests",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html",
-        likes: 10
-    }
-    
-    await api 
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-    const response = await api.get('/api/blogs')
-
-    const contents = response.body.map(r => r.content)
-
-    expect(response.body).toHaveLength(initialBlogs.length + 1)
-    expect(contents).toContain('Second class tests')
-})
-
 afterAll(() => {
     mongoose.connection.close()
 })
