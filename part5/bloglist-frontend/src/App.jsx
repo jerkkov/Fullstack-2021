e0b10b6aebab7b19d37a18597f8374bd577f80f2
Jerkko Viisteensaari <vjerkko@gmail.com>
2024-10-14 14:09:28 +0300

    Add login form

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -4,6 +4,8 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState([])
+  const [password, setPassword] = useState([])
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +13,47 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log(`logging in with ${username} ${password}`)
+  } 
+
   return (
     <div>
-      <h2>blogs</h2>
+      <h2>Blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <section>
+          <label>
+            Username
+            <div>
+          <input
+          type="text"
+          value={username}
+          name="username"
+          id="username"
+          onChange={({ target }) => setUsername(target.value)}
+          />
+          </div>
+        </label>
+
+        <label>Password  
+          <div>
+          <input
+          type="password"
+          value={password}
+          name="password"
+          id="password"
+          onChange={({ target }) => setPassword(target.value)}
+          />
+          </div>
+        </label>
+          <button type="submit">Login</button>
+        </section>
+      </form>      
     </div>
   )
 }
