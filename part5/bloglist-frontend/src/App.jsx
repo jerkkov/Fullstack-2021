066512ead289738632d45d35e41c1f69540c0ead
Jerkko Viisteensaari <vjerkko@gmail.com>
2024-11-14 20:13:10 +0200

    Add 5.3 create new blogs

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -11,6 +11,12 @@ const App = () => {
   const [user, setUser] = useState(null)
   const [notification, setNotification] = useState({})
 
+  const [title, setTitle] = useState(null)
+  const [author, setAuthor] = useState(null)
+  const [url, setUrl] = useState(null)
+  const [likes, setLikes] = useState(null)
+
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
@@ -63,6 +69,27 @@ const App = () => {
     }
     console.log(`logged out`)
   } 
+
+  const handleSaveBlog  = async (event) => {
+    try {
+      const newBlog = await blogService.create({
+        title, author, url, likes
+    })
+      console.log("blog", title, author, url, likes)
+      blogService.setToken(user.token)
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      setLikes('')
+    } catch (exception) {
+      setNotification({type:'error', message:'Wrong credentials'})
+      setTimeout(() => {
+        setNotification({type:'error', message:null})
+      }, 5000)
+    }
+    console.log(`logging in with ${user}`)
+  } 
+
   const loginForm = () => (
     <div>
     <h3>Login</h3>
@@ -98,23 +125,88 @@ const App = () => {
     </div>  
   )
 
+  const saveBlogForm = () => (
+    <div>
+    <h3>Create New</h3>
+    <form onSubmit={handleSaveBlog}>
+      <section>
+        <label>
+          Title
+            <div>
+            <input
+            type="text"
+            value={title}
+            name="title"
+            id="title"
+            onChange={({ target }) => setTitle(target.value)}
+            />
+          </div>
+        </label>
+        <label>
+            Author  
+            <div>
+              <input
+              type="text"
+              value={author}
+              name="author"
+              id="author"
+              onChange={({ target }) => setAuthor(target.value)}
+              />
+            </div>
+          </label>
+          <label>
+            URL  
+            <div>
+              <input
+              type="text"
+              value={url}
+              name="url"
+              id="url"
+              onChange={({ target }) => setUrl(target.value)}
+              />
+            </div>
+        </label>
+        <label>
+            Likes  
+            <div>
+              <input
+              type="number"
+              value={likes}
+              name="likes"
+              id="likes"
+              onChange={({ target }) => setLikes(target.value)}
+              />
+            </div>
+        </label>
+        <button type="submit">Create</button>
+      </section>
+    </form>    
+    </div>  
+  )
+
   const blogForm = () => (
     <div>
     {blogs.map(blog =>
       <Blog key={blog.id} blog={blog} />
     )}
-    <button onClick={handleLogout}>Logout</button>
     </div>
   )
 
   return (
-    <div>
-      <h1>Blogs</h1>
+    <>
+      <table>
+        <thead>
+          <tr>
+        <td><h1>Blogs</h1></td><td><button onClick={handleLogout}>Logout</button></td>
+        </tr>
+        </thead>
+      </table>
       {user && blogForm()}
+      {user && saveBlogForm()}
       {!user && loginForm()}
       <Notification type={notification.type && notification.type} message={notification.message && notification.message} />
 
-    </div>
+    </>
   )
 }
 
