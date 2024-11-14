bc45c7d97e7c2859c02017d77ff6c6b37c89a258
Jerkko Viisteensaari <vjerkko@gmail.com>
2024-11-14 18:51:31 +0200

    Add 5.1 user can login

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState([])
-  const [password, setPassword] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [notification, setNotification] = useState({})
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -13,47 +17,74 @@ const App = () => {
     )  
   }, [])
 
-  const handleLogin = (event) => {
+  const handleLogin  = async (event) => {
     event.preventDefault()
-    console.log(`logging in with ${username} ${password}`)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUsername('')
+      setPassword('')
+      console.log(user)
+      setUser(user)
+    } catch (exception) {
+      setNotification({type:'error', message:'Wrong credentials'})
+      setTimeout(() => {
+        setNotification({type:'error', message:null})
+      }, 5000)
+    }
+    console.log(`logging in with ${user}`)
   } 
+  const loginForm = () => (
+    <div>
+    <h3>Login</h3>
+    <form onSubmit={handleLogin}>
+      <section>
+        <label>
+          Username
+          <div>
+        <input
+        type="text"
+        value={username}
+        name="username"
+        id="username"
+        onChange={({ target }) => setUsername(target.value)}
+        />
+        </div>
+      </label>
+
+      <label>Password  
+        <div>
+        <input
+        type="password"
+        value={password}
+        name="password"
+        id="password"
+        onChange={({ target }) => setPassword(target.value)}
+        />
+        </div>
+      </label>
+        <button type="submit">Login</button>
+      </section>
+    </form>    
+    </div>  
+  )
+
+  const blogForm = () => (
+    <div>
+    {blogs.map(blog =>
+      <Blog key={blog.id} blog={blog} />
+    )}
+    </div>
+  )
 
   return (
     <div>
-      <h2>Blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <section>
-          <label>
-            Username
-            <div>
-          <input
-          type="text"
-          value={username}
-          name="username"
-          id="username"
-          onChange={({ target }) => setUsername(target.value)}
-          />
-          </div>
-        </label>
+      <h1>Blogs</h1>
+      {user && blogForm()}
+      {!user && loginForm()}
+      <Notification type={notification.type && notification.type} message={notification.message && notification.message} />
 
-        <label>Password  
-          <div>
-          <input
-          type="password"
-          value={password}
-          name="password"
-          id="password"
-          onChange={({ target }) => setPassword(target.value)}
-          />
-          </div>
-        </label>
-          <button type="submit">Login</button>
-        </section>
-      </form>      
     </div>
   )
 }
